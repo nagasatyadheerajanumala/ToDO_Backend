72ad7ebe1ba0e077a80233bfb645147bedd5aebf
dheerajanumala <dheeraj@verosai.com>
2025-04-02 19:36:19 -0400

    Revert "To Do app backend final changes"
    This reverts commit 279a07fd8be73f5f5cd4fd2fbba166b002c5c72e.

--- src/main/java/com/spring/To_Do_App/repository/SubTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.To_Do_App.repository;
-
-import com.spring.To_Do_App.model.SubTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubTaskRepository extends JpaRepository<SubTask, Long> {
-}
